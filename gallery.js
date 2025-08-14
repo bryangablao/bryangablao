358ecac5abc5d5718cabc09f9496960b9de88d81
Accxs <Ablaobryan4400@gmail.com>
2025-08-13 22:24:58 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,51 +1,13 @@
-// Gallery categories and images
-const galleryData = {
-    "Friends": [
-        "images/Gallery/Friends/friend1.jpg",
-        "images/Gallery/Friends/friend2.jpg"
-    ],
-    "Family": [
-        "images/Gallery/Family/family1.jpg",
-        "images/Gallery/Family/family2.jpg"
-    ],
-    "Colleagues": [
-        "images/Gallery/Colleagues/colleague1.jpg",
-        "images/Gallery/Colleagues/colleague2.jpg"
-    ],
-    "Me": [
-        "images/Gallery/Me/me1.jpg",
-        "images/Gallery/Me/me2.jpg"
-    ],
-    "Others": [
-        "images/Gallery/Others/other1.jpg",
-        "images/Gallery/Others/other2.jpg"
-    ]
-};
+function openModal(imgElement) {
+    var modal = document.getElementById("imageModal");
+    var modalImg = document.getElementById("modalImg");
+    var downloadBtn = document.getElementById("downloadBtn");
 
-// Show images based on category
-function showCategory(category) {
-    const gallery = document.getElementById("gallery-container");
-    gallery.innerHTML = ""; // Clear previous images
-
-    galleryData[category].forEach(imgSrc => {
-        let img = document.createElement("img");
-        img.src = imgSrc;
-        img.alt = category;
-        img.onclick = () => openModal(imgSrc);
-        gallery.appendChild(img);
-    });
-}
-
-// Modal handling
-function openModal(src) {
-    document.getElementById("imageModal").style.display = "block";
-    document.getElementById("modalImage").src = src;
-    document.getElementById("downloadBtn").href = src;
+    modal.style.display = "block";
+    modalImg.src = imgElement.src;
+    downloadBtn.href = imgElement.src;
 }
 
 function closeModal() {
     document.getElementById("imageModal").style.display = "none";
 }
-
-// Load default category
-showCategory("Friends");
